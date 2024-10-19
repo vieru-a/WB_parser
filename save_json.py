de15aef9ce4ce85efc70ce0ba7ab59ff7ee057c8
vieru-a <vierua2200@gmail.com>
2024-10-19 19:41:40 +0300

    headers и params

---
+++ save_json.py
@@ -0,0 +1,28 @@
+headers = {
+    "accept": "*/*",
+    "accept-language": "ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7",
+    "origin": "https://www.wildberries.ru",
+    "priority": "u=1, i",
+    "referer": "https://www.wildberries.ru/catalog/0/search.aspx?search=%D1%87%D0%B8%D0%BF%D1%81%D1%8B",
+    "sec-ch-ua": '"Google Chrome";v="129", "Not=A?Brand";v="8", "Chromium";v="129"',
+    "sec-ch-ua-mobile": "?1",
+    "sec-ch-ua-platform": '"Android"',
+    "sec-fetch-dest": "empty",
+    "sec-fetch-mode": "cors",
+    "sec-fetch-site": "cross-site",
+    "user-agent": "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Mobile Safari/537.36",
+    "x-captcha-id": "Catalog 1|1|1730382870|AA==|88e02ffa7a964212a001a9065dfb2586|fRjiJehxOFGOi3zGwP38pOKhjzIdwgbLdSY5H9m9AW6",
+    "x-queryid": "qid13067514172495308520241017135433",
+}
+
+params = {
+    "ab_testing": "false",
+    "appType": "1",
+    "curr": "rub",
+    "dest": "-1257786",
+    "query": "",
+    "resultset": "catalog",
+    "sort": "popular",
+    "spp": "30",
+    "suppressSpellcheck": "false",
+}
